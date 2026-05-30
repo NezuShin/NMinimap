71e818fd0bb8861e5c6f29ed49fb1559def91282
ося <111123667+meisosi@users.noreply.github.com>
2026-05-30 21:03:37 +0300

    Update ChunkEntry.java

--- src/main/java/su/nezushin/nminimap/chunks/ChunkEntry.java
+++ src/main/java/su/nezushin/nminimap/chunks/ChunkEntry.java
@@ -1,5 +1,6 @@
 package su.nezushin.nminimap.chunks;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import su.nezushin.nminimap.util.config.Config;
@@ -7,18 +8,17 @@ import su.nezushin.nminimap.util.config.UndergroundLayer;
 
 import java.io.File;
 
-public record ChunkEntry(World w, int x, int z, UndergroundLayer layer) {
-
-    public ChunkEntry(World w, int x, int z) {
-        this(w, x, z, null);
-    }
+public record ChunkEntry(String world, int x, int z, UndergroundLayer layer) {
 
     public File getAsFile() {
         String layerSuffix = layer != null ? "_layer_" + layer.id() : "";
-        return new File(Config.cacheFolder, w.getName() + "." + x + "." + z + layerSuffix + ".bin.gz");
+        return new File(Config.cacheFolder, world + "." + x + "." + z + layerSuffix + ".bin.gz");
     }
 
+    public World getWorld() {//fix for cases where world is not loaded when cache is already there
+        return Bukkit.getWorld(world);
+    }
     public boolean isInsideWorldBorder() {
-        return w.getWorldBorder().isInside(new Location(w, x * 16, 0, z * 16));
+        return getWorld().getWorldBorder().isInside(new Location(getWorld(), x * 16, 0, z * 16));
     }
 }
