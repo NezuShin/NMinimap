dc965aa56243167a7de37610c04a1d174821cbca
DartCat25 <vlaadmai005@gmail.com>
2026-09-14 22:49:13 +0300

    Fix absolute offset view scaling

--- src/main/resources/defaults/shaders/include/config.glsl
+++ src/main/resources/defaults/shaders/include/config.glsl
@@ -2,8 +2,8 @@
 // #define MAP_OFFSET vec2(0.05025, 0.05025)
 
 #define MAP_ABSOLUTE_SIZES
-#define MAP_OFFSET vec2(60, 60)
-#define MAP_SIZE vec2(128*6)
+#define MAP_OFFSET vec2(30, 30)
+#define MAP_SIZE vec2(128*3)
 
 #define MAP_CONTENT_SIZE {content}
 #define MAP_CROP_RADIUS (MAP_CONTENT_SIZE/2.0)
